7710eae541e54413c711ea9479be6faa2e4cb4f3
Daubert <jzh16s@hotmail.com>
2019-07-19 00:34:15 +0800

    ADD: 回文链表

---
+++ Sources/GiveMeAnOffer/LeetCode/234.回文链表.swift
@@ -0,0 +1,49 @@
+//
+// Created by daubert on 19-7-18.
+//
+
+import Foundation
+
+public extension ListNode {
+
+    static func isPalindrome(_ head: ListNode?) -> Bool {
+        return head?.isPalindrome() ?? true
+    }
+
+    func isPalindrome() -> Bool {
+        guard next != nil else {
+            return true
+        }
+
+        var slow: ListNode? = self, fast: ListNode? = self
+        while fast?.next != nil && fast?.next?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+
+        var last = slow?.next, pre: ListNode? = self
+        while last?.next != nil {
+            let tmp = last?.next
+            last?.next = tmp?.next
+            tmp?.next = slow?.next
+            slow?.next = tmp
+        }
+
+        while slow?.next != nil {
+            slow = slow?.next
+            if let s = slow?.val,
+               let p = pre?.val {
+                if s != p {
+                    return false
+                }
+            } else if slow != nil {
+                return false
+            } else {
+                return false
+            }
+            pre = pre?.next
+        }
+
+        return true
+    }
+}
