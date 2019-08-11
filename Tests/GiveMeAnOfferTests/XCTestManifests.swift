da22f469994c8c4de0ec1531b0445b645914c847
AmatsuZero <jzh16s@hotmail.com>
2019-08-11 10:58:34 +0800

    CHG: update .gitignore

--- Tests/GiveMeAnOfferTests/XCTestManifests.swift
+++ Tests/GiveMeAnOfferTests/XCTestManifests.swift
@@ -42,7 +42,7 @@ public func allTests() -> [XCTestCaseEntry] {
         testCase(ConstructRectangleTests.allTests),
         testCase(LicenseKeyFormattingTests.allTests),
         testCase(BinarySearchTreeTests.allTests),
-//        testCase(TrieTests.allTests),
+        testCase(TrieTests.allTests),
         testCase(EvalPRNTests.allTests),
         testCase(LRUCacheTests.allTests)
     ]
